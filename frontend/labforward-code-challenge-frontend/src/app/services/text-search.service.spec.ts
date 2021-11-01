42383c54abfa19e2aeb015fe7b5e09fd1bfe0b9e
Paul Olabisi <polabisi@paga.com>
2021-11-02 00:34:51 +0100

    added service test

--- frontend/labforward-code-challenge-frontend/src/app/services/text-search.service.spec.ts
+++ frontend/labforward-code-challenge-frontend/src/app/services/text-search.service.spec.ts
@@ -1,16 +1,50 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { TextSearchService } from './text-search.service';
+import { TextSearchRequest } from '../models/TextSearchRequest';
+import { TextSearchResponse } from '../models/TextSearchResponse';
+import { environment } from 'src/environments/environment';
 
 describe('TextSearchService', () => {
   let service: TextSearchService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+
     service = TestBed.inject(TextSearchService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#getWordFrequencyAndSimilarWords should return word frequency and return similar words', () => {
+
+    const request: TextSearchRequest = { searchWord: "Word", notepadText: "Word words wor" };
+
+    const expectedResponse: TextSearchResponse = {
+      wordFrequency: 1,
+      similarWords: ["word", "word", "words"]
+    }
+
+    service.getWordFrequencyAndSimilarWords(request).subscribe(data => {
+      expect(data).toEqual(expectedResponse);
+    });
+
+    const testRequest = httpTestingController.expectOne({ method: "POST", url: environment.getWordFrequencyAndSimilarWords });
+    testRequest.flush(expectedResponse);
+
+  });
+  
 });
